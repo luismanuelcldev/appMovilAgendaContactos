f36842837792d19a07a38910afae6620a5caab3d
Luis Manuel De La Cruz <luismanueldelacruz0000@gmail.com>
2025-09-03 14:37:07 -0400

    Implementar servicio API para obtener contactos aleatorios desde la API 'randomuser.me

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,36 +1,47 @@
+// Importar paquetes necesarios
 import 'package:dio/dio.dart';
 
 class ApiService {
   final Dio _dio = Dio(
     BaseOptions(
+      // URL de la API
       baseUrl: 'https://randomuser.me/api/',
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 5),
+      connectTimeout: const Duration(
+        seconds: 5,
+      ), // Tiempo de espera para conexión
+      receiveTimeout: const Duration(
+        seconds: 5,
+      ), // Tiempo de espera para recibir la respuesta
     ),
   );
 
   Future<List<Map<String, dynamic>>> obtenerContactos({
-    int cantidad = 20,
+    int cantidad = 20, // Cantidad de contactos a obtener
   }) async {
     try {
       final response = await _dio.get(
         '',
+        // Parámetro para resultados
         queryParameters: {
           'results': cantidad,
+          // Campos a incluir
           'inc': 'name,email,phone,picture,login,dob,location',
           'nat': 'es',
-          'seed': 'espanol', // Para consistencia en los datos
+          'seed': 'espanol',
         },
       );
 
+      // Convertir resultados a lista
       final results = List<Map<String, dynamic>>.from(response.data['results']);
 
+      // Asignar valor si no hay teléfono
       for (var contacto in results) {
         if (contacto['phone'] == null) {
           contacto['phone'] = 'No disponible';
         }
       }
 
+      // Retornar lista de contactos
       return results;
     } catch (e) {
       throw Exception('Error al obtener contactos: ${e.toString()}');
