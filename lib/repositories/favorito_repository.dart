58a52213767d41474308428576a346eebe283695
Luis Manuel De La Cruz <luismanueldelacruz0000@gmail.com>
2025-09-03 14:24:39 -0400

    Crear favorito_repository.dart para gestionar favoritos de contactos

--- lib/repositories/favorito_repository.dart
+++ lib/repositories/favorito_repository.dart
@@ -1,13 +1,17 @@
-import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../models/favorito.dart';
+import 'dart:convert'; // Importa la biblioteca para trabajar con JSON.
+import 'package:shared_preferences/shared_preferences.dart'; // Importa la biblioteca para manejar preferencias compartidas.
+import '../models/favorito.dart'; // Importa el modelo de Favorito.
 
 class FavoritoRepository {
-  final SharedPreferences _prefs;
+  final SharedPreferences
+  _prefs; // Instancia de SharedPreferences para el almacenamiento de datos.
   static const String _key = 'favoritos';
 
-  FavoritoRepository(this._prefs);
+  FavoritoRepository(
+    this._prefs,
+  ); // Constructor que inicializa SharedPreferences.
 
+  // Obtiene la lista de favoritos almacenados.
   List<Favorito> obtenerFavoritos() {
     final favoritosJson = _prefs.getStringList(_key) ?? [];
     return favoritosJson
@@ -15,6 +19,7 @@ class FavoritoRepository {
         .toList();
   }
 
+  // Agregar un nuevo favorito si no existe ya en la lista.
   Future<void> agregarFavorito(String contactoId) async {
     final favoritos = obtenerFavoritos();
     if (!favoritos.any((f) => f.contactoId == contactoId)) {
@@ -25,19 +30,24 @@ class FavoritoRepository {
     }
   }
 
+  // Elimina un favorito de la lista.
   Future<void> eliminarFavorito(String contactoId) async {
     final favoritos = obtenerFavoritos();
     favoritos.removeWhere((f) => f.contactoId == contactoId);
     await _guardarFavoritos(favoritos);
   }
 
+  // Limpia todos los favoritos almacenados.
   Future<void> limpiarFavoritos() async {
     await _prefs.remove(_key);
   }
 
+  // Guarda la lista de favoritos en formato JSON.
   Future<void> _guardarFavoritos(List<Favorito> favoritos) async {
     final favoritosJson =
-        favoritos.map((favorito) => jsonEncode(favorito.toJson())).toList();
+        favoritos
+            .map((favorito) => jsonEncode(favorito.toJson()))
+            .toList(); // Convierte cada Favorito a JSON.
     await _prefs.setStringList(_key, favoritosJson);
   }
 }
