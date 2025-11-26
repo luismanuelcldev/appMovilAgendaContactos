fcd4a14a03f954265d8fe7b0d430f13e68dc310f
Luis Manuel De La Cruz Ledesma <luismanueldelacruz0000@gmail.com>
2025-11-26 12:44:06 -0400

    refactor: Mejorar la claridad de los comentarios en favorito.dart

--- lib/models/favorito.dart
+++ lib/models/favorito.dart
@@ -1,24 +1,23 @@
+// Clase que representa un contacto marcado como favorito
 class Favorito {
-  // Identificador unico del contacto asociado a este favorito.
+  // Identificador único del contacto asociado a este favorito
   final String contactoId;
 
-  // Fecha y hora en que se agrego este favorito.
+  // Fecha y hora en que se agregó este favorito
   final DateTime agregadoEn;
 
-  // Constructor para crear una instancia de Favorito.
+  // Constructor para crear una instancia de Favorito
   Favorito({required this.contactoId, required this.agregadoEn});
 
-  // Convierto la instancia de Favorito a un mapa JSON.
+  // Convierto la instancia de Favorito a un mapa JSON para almacenamiento
   Map<String, dynamic> toJson() => {
     'contactoId': contactoId,
-
     'agregadoEn': agregadoEn.toIso8601String(),
   };
 
-  // Creo una instancia de Favorito a partir de un mapa JSON.
+  // Creo una instancia de Favorito a partir de un mapa JSON
   factory Favorito.fromJson(Map<String, dynamic> json) => Favorito(
     contactoId: json['contactoId'],
-
     agregadoEn: DateTime.parse(json['agregadoEn']),
   );
 }
