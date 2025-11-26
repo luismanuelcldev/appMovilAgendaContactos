edfd9b25321df31cef6ae2245296f602a02b912e
Luis Manuel De La Cruz Ledesma <luismanueldelacruz0000@gmail.com>
2025-11-26 12:47:42 -0400

    refactor: Añadir comentarios para mejorar la claridad del widget ThemeSwitch

--- lib/widgets/theme_switch.dart
+++ lib/widgets/theme_switch.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../cubit/agenda_cubit.dart';
 
+// Widget que permite alternar entre tema claro y oscuro
 class ThemeSwitch extends StatelessWidget {
   const ThemeSwitch({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Escucho el estado del Cubit para reflejar el tema actual
     return BlocBuilder<AgendaCubit, AgendaState>(
       builder: (context, state) {
+        // Switch que cambia el tema al ser activado/desactivado
         return Switch(
           value: state.temaOscuro,
           onChanged: (value) => context.read<AgendaCubit>().cambiarTema(value),
